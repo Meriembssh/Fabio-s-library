8442df86975bd772cc46a9b0b1364f1fae229e6c
Meriembssh <daisybssh@gmail.com>
2022-07-31 17:56:03 +0100

    update3

--- routes/authors.js
+++ routes/authors.js
@@ -3,9 +3,21 @@ const Author = require('../models/author')
 const router = express.Router()
 
 // All authors:
-router.get('/', (req,res) => {
-    res.render('authors/index')
-})
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+      searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+      const authors = await Author.find(searchOptions)
+      res.render('authors/index', {
+        authors: authors,
+        searchOptions: req.query
+      })
+    } catch {
+      res.redirect('/')
+    }
+  })
 
 // New author:
 router.get('/new', (req,res) => {
